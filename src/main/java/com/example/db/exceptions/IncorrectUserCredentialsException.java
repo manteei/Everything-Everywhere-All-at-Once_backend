214068fc12823b17db1d7f436b985d6d62aff029
Gromilova Maria <95652195+manteei@users.noreply.github.com>
2024-07-31 17:49:49 +0300

    Update UserCredentials

--- src/main/java/com/example/db/exceptions/IncorrectUserCredentialsException.java
+++ src/main/java/com/example/db/exceptions/IncorrectUserCredentialsException.java
@@ -1,5 +1,4 @@
 package com.example.db.exceptions;
-
 public class IncorrectUserCredentialsException extends RuntimeException{
     public IncorrectUserCredentialsException(String massage) {
         super(massage);
